0d73363635d9f68f57521b3d8e34aba1210aab0e
xiazhipeng <xiazhipeng@bytedance.com>
2019-11-26 17:15:10 +0800

    create

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+// 代理
+app.get('/index', (req, res) => {
+  const page = require('fs').readFileSync('./index.html', 'utf-8');
+  res.end(page);
+})
+app.get('/', (req, res) => {
+  const page = require('fs').readFileSync('./index.html', 'utf-8');
+  res.end(page);
+})
+app.get('/video', (req, res) => {
+  const video = require('fs').readFileSync('./video.mp4');
+  res.end(video);
+})
+app.get('/jquery', (req, res) => {
+  const jquery = require('fs').readFileSync('../assets/js/jquery-3.4.1.min.js', 'utf-8');
+  res.end(jquery);
+})
+app.listen(998, () => console.log('Example app listening on port 998!'))
